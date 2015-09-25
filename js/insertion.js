a3c0712411aafb6bb87b1c70004254c804dfb6ff
Alexander Anich <alexander.anich@gmail.com>
2015-09-25 10:42:01 -1000

    document insertionSort

--- js/insertion.js
+++ js/insertion.js
@@ -1,16 +1,33 @@
 function insertionSort (array) {
-  var current;
+
+  //initialize a variable to identify the number currently being compared
+  var value;
+
+  //loop through our  array
   for (var i = 0; i < array.length; i++) {
-    current = array[i];
+
+    //indentify our number to be compared
+    value = array[i];
+
+    //create a variable to compare the previous number
     var j = i - 1;
-    while (j > -1 && array[j] > current) {
+
+    //while our previous number is greater than 0, and the number we're comparing is less
+    //than our previous number enter our loop
+    while (j > -1 && array[j] > value) {
+
+      //shift the number down the array and give us a space to insert
+      //our current value
       array[j + 1] = array[j];
+
+      //decrement j to go through our entire array
       j--;
     }
-    array[j + 1] = current;
+
+    //insert our value at the space that has been shifted
+    array[j + 1] = value;
   }
 
+  //return the sorted array
   return array;
 }
-
-module.exports = insertionSort;
