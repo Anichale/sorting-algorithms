310810a83eda64d6de4462a837d3c32633414e2c
Alexander Anich <alexander.anich@gmail.com>
2015-09-25 10:52:45 -1000

    implement selectionSort

--- js/selection.js
+++ js/selection.js
@@ -1,3 +1,18 @@
-function selection (array) {
-
+function selectionSort (array) {
+  var min;
+  var prev;
+  for (var i = 0; i < array.length; i++) {
+    min = i;
+    for (var j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      prev = array[i];
+      array[i] = array[min];
+      array[min] = prev;
+    }
+  }
+  return array;
 }
