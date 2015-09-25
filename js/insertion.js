c657a2276fb63eccec607861dc8dc3217eb20a17
Alexander Anich <alexander.anich@gmail.com>
2015-09-25 10:33:54 -1000

    implement insertioSort

--- js/insertion.js
+++ js/insertion.js
@@ -1,5 +1,16 @@
-function insertion (array) {
+function insertionSort (array) {
+  var current;
+  for (var i = 0; i < array.length; i++) {
+    current = array[i];
+    var j = i - 1;
+    while (j > -1 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
 
+  return array;
 }
 
-module.exports = insertion;
+module.exports = insertionSort;
