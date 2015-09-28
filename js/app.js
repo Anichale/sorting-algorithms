135bbc7ea92866c5453ab6f590832860f25587d4
Alexander Anich <alexander.anich@gmail.com>
2015-09-28 10:55:48 -1000

    changed formatting in app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
+var cr = {
 
+  button : function createButton (action, name) {
+    var button = document.createElement('button');
+    button.innerHTML = name;
+    button.style.margin = '10px';
+    button.setAttribute('onclick', action);
+    document.getElementById('visualizer').appendChild(button);
+  },
 
-function createButton (action, name) {
-  var button = document.createElement('button');
-  button.innerHTML = name;
-  button.style.margin = '10px';
-  button.setAttribute('onclick', action);
-  document.getElementById('visualizer').appendChild(button);
-}
+  reset : function reset () {
+    shuffledArray = DataArray.getRandomArray();
+    draw.grid(shuffledArray);
+  }
 
-function reset () {
-  shuffledArray = DataArray.getRandomArray();
-  draw.grid(shuffledArray);
-}
+};
 
-createButton('Sort.bubble(shuffledArray)', 'Bubble Sort');
-createButton('Sort.insertion(shuffledArray)', 'Insertion Sort');
-createButton('Sort.Selection(shuffledArray)', 'Selection Sort');
-createButton('Sort.quick(shuffledArray)', 'Quick Sort');
-createButton('reset()', 'Reset');
+cr.button('Sort.bubble(shuffledArray)', 'Bubble Sort');
+cr.button('Sort.insertion(shuffledArray)', 'Insertion Sort');
+cr.button('Sort.Selection(shuffledArray)', 'Selection Sort');
+cr.button('Sort.quick(shuffledArray)', 'Quick Sort');
+cr.button('cr.reset()', 'Reset');
 var shuffledArray = DataArray.getRandomArray();
 draw.grid(shuffledArray);
 
-//Sort.bubble(shuffledArray);
-//Sort.insertion(shuffledArray);
 //Sort.merge(shuffledArray);
-//Sort.Selection(shuffledArray);
-//Sort.quick(shuffledArray);
